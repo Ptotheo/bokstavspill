41b5239195cc3a268a7004eefd1ffe17db1acc65
Ptotheo <pollestad92@gmail.com>
2020-12-16 07:37:12 +0100

    Initial commit

---
+++ bspill.py
@@ -0,0 +1,15 @@
+from playsound import playsound
+import soundfile as sf
+
+
+
+playsound(a.wav)
+sound_a = input("Hvilken bokstav er dette :")
+
+
+def a():
+	if sound_a == a:
+		print("Tipp Topp Tommel Opp!")
+	else: 
+		print("prøv igjen")
+
